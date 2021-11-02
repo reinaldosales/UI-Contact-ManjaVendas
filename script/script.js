0937d87807d59f8b56bed63f0562a0c07cc80bb7
Reinaldo Sales <httpsreinaldo@gmail.com>
2021-11-02 15:38:05 -0300

    Alteração da validação dos campos

--- script/script.js
+++ script/script.js
@@ -1,69 +1,44 @@
 $(document).ready(function() {
-
-    $('#button').click(function(e) {
-        e.preventDefault();
-
-        $('#name_error').html('');
-        $('#email_error').html('');
-        $('#phone_error').html('');
-        $('#name').removeClass('input_empty');
-        $('#email').removeClass('input_empty');
-        $('#phone').removeClass('input_empty');
-
-
-        let name = $.trim($('#name').val());
-        let email = $.trim($('#email').val());
-        let phone = $.trim($('#phone').val());
-        let message = $.trim($('#message').val());
-
-        if (name === '') {
-            $('#name_error').html('Campo não pode estar vazio.');
-            $('#name').addClass('input_empty');
-        }
-
-        if (email === '') {
-            $('#email_error').html('Campo não pode estar vazio.');
-            $('#email').addClass('input_empty');
-        }
-
-        if (phone === '') {
-            $('#phone_error').html('Campo não pode estar vazio.');
-            $('#phone').addClass('input_empty');
+    $('#card_form').on('submit', function(e) {
+        if (!validate()) {
+            e.preventDefault();
         }
     });
 });
 
-// $(document).ready(function() {
-//     $('#card_form').on('submit', function() {
-//         console.log('oi');
+function validate() {
+    let validator = true;
 
-//         $('#name_error').html('');
-//         $('#email_error').html('');
-//         $('#phone_error').html('');
-//         $('#name').removeClass('input_empty');
-//         $('#email').removeClass('input_empty');
-//         $('#phone').removeClass('input_empty');
+    $('#name_error').html('');
+    $('#email_error').html('');
+    $('#phone_error').html('');
+    $('#name').removeClass('input_empty');
+    $('#email').removeClass('input_empty');
+    $('#phone').removeClass('input_empty');
 
 
-//         let name = $.trim($('#name').val());
-//         let email = $.trim($('#email').val());
-//         let phone = $.trim($('#phone').val());
-//         let message = $.trim($('#message').val());
+    let name = $.trim($('#name').val());
+    let email = $.trim($('#email').val());
+    let phone = $.trim($('#phone').val());
+    let message = $.trim($('#message').val());
 
-//         if (name === '') {
-//             $('#name_error').html('Campo não pode estar vazio.');
-//             $('#name').addClass('input_empty');
-//         }
+    if (name === '') {
+        $('#name_error').html('Campo não pode estar vazio.');
+        $('#name').addClass('input_empty');
+        validator = false;
+    }
 
-//         if (email === '') {
-//             $('#email_error').html('Campo não pode estar vazio.');
-//             $('#email').addClass('input_empty');
-//         }
+    if (email === '') {
+        $('#email_error').html('Campo não pode estar vazio.');
+        $('#email').addClass('input_empty');
+        validator = false;
+    }
 
-//         if (phone === '') {
-//             $('#phone_error').html('Campo não pode estar vazio.');
-//             $('#phone').addClass('input_empty');
-//         }
-//     });
+    if (phone === '') {
+        $('#phone_error').html('Campo não pode estar vazio.');
+        $('#phone').addClass('input_empty');
+        validator = false;
+    }
 
-// });
+    return validator;
+}
